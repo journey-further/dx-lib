7e2cb55a9e749294bd08f2a4281d4ea6732312f9
Paul Singh <im.makingstuffs@gmail.com>
2022-01-20 09:45:25 +0000

    Add replaceHTML [20 - 28] -- Simple function use instead of .innerHTML, it empties an element of all nodes then inserts the string provided as parameter 2

--- src/modules/elementModifiers.module.ts
+++ src/modules/elementModifiers.module.ts
@@ -16,3 +16,13 @@ export const emptyElem = (elem: HTMLElement): void => {
     console.log(e);
   }
 };
+
+export const replaceHTML = (elem: HTMLElement, html: string): void => {
+  if (!!!elem) throw new Error("You did not provide an element");
+  if (!(elem instanceof HTMLElement))
+    throw Error("Parameter 1 must be a HTML element");
+  if (!!!html) throw new Error("You did not provide any HTML markup");
+  if (typeof html !== "string") throw Error("Parameter 2 must be a string");
+  emptyElem(elem);
+  elem.insertAdjacentHTML("afterbegin", html);
+};
