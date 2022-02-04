ae9eaee25321f4e263d6645a2e562cdda30c5aa4
Paul Singh <im.makingstuffs@gmail.com>
2022-02-04 09:54:35 +0000

    Rebuild

--- dist/modules/deviceIdentifiers.module.js
+++ dist/modules/deviceIdentifiers.module.js
@@ -1,10 +1,2 @@
 export const isIphone = () => /iPhone/i.test(navigator.userAgent);
 export const isMobile = () => /iPhone|Android|Opera Mini|Blackberry|Windows Phone|IEMobile/i.test(navigator.userAgent);
-export const insertStyle = (style, ticket) => {
-    if (!!!document.querySelector('#'+ticket)) {
-      document.body.insertAdjacentHTML(
-        "beforeend",
-        `<style id="`+ticket+`">${style.toString()}</style>`
-      );
-    }
-  };
--- dist/modules/documentModifiers.module.d.ts
+++ dist/modules/documentModifiers.module.d.ts
@@ -1,10 +1,2 @@
 export declare const preventScroll: () => void;
 export declare const enableScroll: () => void;
-export const insertStyle = (style: string, ticket: string): void => {
-    if (!!!document.querySelector('#' + ticket)) {
-        document.body.insertAdjacentHTML(
-            "beforeend",
-            `<style id="` + ticket + `">${style.toString()}</style>`
-        );
-    }
-};
