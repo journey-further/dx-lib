c7101c6f500287485d864de01a9a735b8be0adff
Paul Singh <im.makingstuffs@gmail.com>
2022-05-23 08:12:02 +0100

    Add tests for queryAll

--- __tests__/modules/getters.module.spec.ts
+++ __tests__/modules/getters.module.spec.ts
@@ -1,6 +1,8 @@
-import { waitFor } from "../../src";
+import { waitFor, queryAll } from "../../src";
 
 ("use strict");
+
+const MOCK_QUERY = ".mock";
 
 // We need to mock our timers
 jest.useRealTimers();
@@ -53,5 +55,20 @@ describe("waitFor", () => {
     expect(setTimeout).toHaveBeenCalledTimes(20);
     expect((setTimeout as unknown as jest.Mock).mock.calls[0][1]).toBe(2); // First recursion should be 1 + 1
     expect((setTimeout as unknown as jest.Mock).mock.calls[19][1]).toBe(21); // Last recursion should be 20 + 1
+  });
+});
+
+describe("queryAll", () => {
+  it("will return a true array", () => {
+    const result = queryAll(MOCK_QUERY);
+    expect(Array.isArray(result)).toBe(true);
+    expect(result.reduce).toBeDefined();
+  });
+
+  it("will call querySelectorAll with the provided argument", () => {
+    const spy = jest.spyOn(document, "querySelectorAll");
+    queryAll(MOCK_QUERY);
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(MOCK_QUERY);
   });
 });
