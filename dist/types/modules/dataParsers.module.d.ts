e2286c2382d6b8aabcd21ae7c5742b22e28889af
Paul Singh <im.makingstuffs@gmail.com>
2022-05-23 08:14:34 +0100

    Rebuild

---
+++ dist/types/modules/dataParsers.module.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Convert the passed JSON object into a FormData object
+ * @param {Object} json The object you wish to parse
+ * @returns {FormData}
+ */
+export declare const parseJsonToFormData: (json: {
+    [key: string]: unknown;
+}) => FormData;
+//# sourceMappingURL=dataParsers.module.d.ts.map
