ea5bb8b7a61624a0c0e51d253c892e85761ff7a8
Paul Singh <im.makingstuffs@gmail.com>
2022-08-15 15:35:39 +0100

    Update: Create dy specific branch

--- dist/modules/dataParsers.module.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Convert the passed JSON object into a FormData object
- * @param {Object} json The object you wish to parse
- * @returns {FormData}
- */
-export declare const parseJsonToFormData: (json: {
-    [key: string]: unknown;
-}) => FormData;
-//# sourceMappingURL=dataParsers.module.d.ts.map
--- dist/modules/navigatorModifiers.module.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Redirect the current webpage to the URL which as been passed as an argument.
- * @param {string} url The URL to redirect to
- * @returns {void}
- */
-export declare const redirect: (url: string) => void;
-//# sourceMappingURL=navigatorModifiers.module.d.ts.map
