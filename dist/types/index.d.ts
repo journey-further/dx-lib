ef1de2c9079f6085d4b91f92c2ccc04c6a03374c
Paul Singh <im.makingstuffs@gmail.com>
2022-09-21 15:35:54 +0100

    Remove: Remove dist as we are using the prepare script to build on packahing

--- dist/types/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./modules/index";
-//# sourceMappingURL=index.d.ts.map
--- dist/types/modules/dateModifiers.module.d.ts
+++
@@ -1,8 +0,0 @@
-import { ParsedTimeObject } from "../types/index";
-/**
- * Return an object with the number of days, hours, minutes and seconds until the provided timestamp is met
- * @param { number } timestamp -- The timestamp you wish to countdown to
- * @returns { ParsedTimeObject } -- An object containing days hours and minutes until provided timestamp
- */
-export declare const getTimeTo: (timestamp: number) => ParsedTimeObject;
-//# sourceMappingURL=dateModifiers.module.d.ts.map
