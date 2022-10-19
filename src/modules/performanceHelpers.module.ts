13bd3474acf25a791d6a1559699ba58cd136704c
samrenfrew <contact@samrenfrew.co.uk>
2022-10-19 12:58:24 +0100

    Add comment to debounce

--- src/modules/performanceHelpers.module.ts
+++ src/modules/performanceHelpers.module.ts
@@ -1,3 +1,9 @@
+/**
+ * Delay a callback function by a set amount
+ * @param callback Function to run
+ * @param delay Time in ms to delay (default: 200)
+ * @returns
+ */
 export function debounce(callback, delay = 200) {
   let timer: ReturnType<typeof setTimeout>;
   return (...args: unknown[]) => {
