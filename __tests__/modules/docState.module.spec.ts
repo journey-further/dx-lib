7a4e6ff3aa01153905c6854baca6834c95c920d2
Paul Singh <im.makingstuffs@gmail.com>
2022-12-08 11:55:20 +0000

    🚧 Add review noted for Barry on the isInDom method

--- __tests__/modules/docState.module.spec.ts
+++ __tests__/modules/docState.module.spec.ts
@@ -99,11 +99,34 @@ describe("docReady", () => {
   });
 });
 
-
 describe("isInDom", () => {
+  /**
+   * We will want a function here to empty the DOM afterEach test.
+   *
+   * Conveniently there is a helper function called `afterEach` which will run after every test.
+   *
+   * It must be placed within this describe block so you can put it directly beneath this comment block.
+   *
+   * The function is:
+   *
+   * AfterEach(() => { // The stuff you want to do after each test // This is where you want to empty the dom });
+   */
   it("Will fail if a dom element doesn't exist", async () => {
-    const dom = 
-    `
+    /**
+     * For these tests we do not need to spoof a dom as the environment set in the Jest config is jsdom This means all
+     * the native dom methods should be available for us in the global scope and the tests, when run, are run with the
+     * dom from this module.
+     *
+     * I have rewritten this test to give you an example of what I mean and you can apply this to the other tests in the
+     * page.
+     *
+     * As you are allowing a dom to be passed to the function you will need to add another test to see if the test will
+     * correctly use the provided dom over the global dom. For that you will need to use your spoofed dom.
+     *
+     * Let me know if you need more clarification bro :)
+     */
+
+    /*const dom = `
       <html> 
       <head>
       </head>
@@ -111,18 +134,21 @@ describe("isInDom", () => {
           <div id="element"></div>
       </body>
       </html>
-    `;
+    `;*/
+    //const doc = new DOMParser().parseFromString(dom, "text/xml");
+    //const element = doc.querySelector("#element").cloneNode(true);
+    //const check = isInDom(element, doc);
+    //expect(check).toBe(false);
 
-    const doc = new DOMParser().parseFromString(dom, "text/xml");
-    const element = doc.querySelector('#element').cloneNode(true);
+    // Create an element
+    const element = document.createElement("div");
 
-    const check = isInDom(element, doc);
-    expect(check).toBe(false);
+    // Check if it is in the dom
+    expect(isInDom(element)).toBe(false);
   });
 
-    it("Will pass if a dom does exist", async () => {
-    const dom = 
-    `
+  it("Will pass if a dom does exist", async () => {
+    const dom = `
       <html> 
       <head>
       </head>
@@ -133,11 +159,9 @@ describe("isInDom", () => {
     `;
 
     const doc = new DOMParser().parseFromString(dom, "text/xml");
-    const element = doc.querySelector('#element');
+    const element = doc.querySelector("#element");
 
     const check = isInDom(element, doc);
     expect(check).toBe(true);
   });
-
-  
 });
--- src/modules/docState.module.ts
+++ src/modules/docState.module.ts
@@ -1,4 +1,10 @@
-// Doc ready function
+/**
+ * Async function to allow us to wait for the document to be in the 'complete' state
+ *
+ * @param maxAttempts The maximum number of times the function should check
+ * @param timeout The time between each check
+ * @returns Whether the doc is in the ready state
+ */
 export const docReady = async (maxAttempts = 10, timeout = 200): Promise<boolean> => {
   let attempts = 0;
   if (document.readyState === "complete") return true;
@@ -14,7 +20,18 @@ export const docReady = async (maxAttempts = 10, timeout = 200): Promise<boolean
   return true;
 };
 
-export const isInDom = (element, dom?): Boolean => {
-  let doc = dom ?? document;
-  return !!element && doc.documentElement.contains(element);
-}
+/**
+ * Check whether the provided element is contained within the current DOM. If a second param is passed it will be used
+ * as the DOM.
+ *
+ * @param element -- The element to check
+ * @param dom -- The dom to check in
+ * @returns Whether or not the element is in the current dom
+ */
+export const isInDom = (
+  element /* What type is this? Should be HTMLElement */,
+  dom? /* We should allow the user to use the function without passing a dom. To do this we can use a default param dom = document */
+): boolean => {
+  const doc = dom ?? document; // Delete this line with the default param
+  return !!element && doc.documentElement.contains(element); // Remove the safety check and let it throw if the function was called without an element param
+};
