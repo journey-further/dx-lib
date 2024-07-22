511a87245b42e87029337fbfbaa0b7096f93060c
Sam Renfrew <contact@samrenfrew.co.uk>
2024-07-22 14:19:48 +0100

    Fix: force user to add id to elementReady

--- src/modules/elementReady.ts
+++ src/modules/elementReady.ts
@@ -1,4 +1,3 @@
-import { generateId } from "./generateId";
 import { useMutationObserver } from "./useMutationObserver";
 import { waitFor } from "./waitFor";
 
@@ -19,7 +18,7 @@ declare global {
 export const elementReady = (
   selector: string,
   callback: (el: Element) => void,
-  id = generateId(),
+  id: string,
   conditions = (el: Element) => !!el
 ) => {
   if (!!!selector) {
@@ -30,10 +29,9 @@ export const elementReady = (
     throw new Error("Callback is not defined");
   }
 
-  // NOTE: reverting this change to make it generate a random one if not provided
-  // if (!!!id) {
-  //   throw new Error("No id provided");
-  // }
+  if (!!!id || id == "") {
+    throw new Error("No id provided");
+  }
 
   const loopDom = () => {
     const targets = document.querySelectorAll(selector);
@@ -71,6 +69,4 @@ export const elementReady = (
   // 2. bind an MO to listen for any future changes
   // eslint-disable-next-line
   bindObserver();
-
-  // stupid fucking commit messages
 };
