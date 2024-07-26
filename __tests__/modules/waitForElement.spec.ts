2757708d06e87a8c2aaebbddb5a44cddc76eefaa
Sam Renfrew <contact@samrenfrew.co.uk>
2024-07-26 15:02:27 +0100

    fix: Update waitForElement to just run waitFor rather than re-defining the function

--- __tests__/modules/waitForElement.spec.ts
+++ __tests__/modules/waitForElement.spec.ts
@@ -19,12 +19,12 @@ describe("waitForElement", () => {
   });
 
   it("will return the truthy value", async () => {
-		// Insert a div with class of mock
-		document.body.insertAdjacentHTML("afterbegin", `<div class="mock"></div>`)
-		const querySelector = jest.spyOn(document, "querySelector")
+    // Insert a div with class of mock
+    document.body.insertAdjacentHTML("afterbegin", `<div class="mock"></div>`);
+    const querySelector = jest.spyOn(document, "querySelector");
     const result = await waitForElement(MOCK_QUERY);
-		expect(querySelector).toHaveBeenCalled(); // Should have called document.querySelector
-    expect(result).toBeDefined(); 
+    expect(querySelector).toHaveBeenCalled(); // Should have called document.querySelector
+    expect(result).toBeDefined();
     expect(result instanceof HTMLElement).toBe(true);
     expect(setTimeout).toBeCalledTimes(0);
   });
@@ -39,23 +39,5 @@ describe("waitForElement", () => {
     await waitForElement(MOCK_QUERY, 50, 1); // set poll to 1s as we don't care about that
     expect(setTimeout).toHaveBeenCalledTimes(50);
     // expect(callback).toHaveBeenCalledTimes(50);
-  });
-
-  it("will call timeout with the correct default poll values", async () => {
-    waitForElement(MOCK_QUERY);
-    for (let i = 0; i < 20; i++) {
-      jest.advanceTimersByTime(10000);
-      await Promise.resolve();
-    }
-    expect(setTimeout).toHaveBeenCalledTimes(20);
-    expect((setTimeout as unknown as jest.Mock).mock.calls[0][1]).toBe(200); // First recursion should be 100 + 100
-    expect((setTimeout as unknown as jest.Mock).mock.calls[19][1]).toBe(2100); // Last recursion should be 2100
-  });
-
-  it("will call timeout with the correct provided poll values", async () => {
-    await waitForElement(MOCK_QUERY, undefined, 1); // set poll to 1s as we dont care about that
-    expect(setTimeout).toHaveBeenCalledTimes(20);
-    expect((setTimeout as unknown as jest.Mock).mock.calls[0][1]).toBe(2); // First recursion should be 1 + 1
-    expect((setTimeout as unknown as jest.Mock).mock.calls[19][1]).toBe(21); // Last recursion should be 20 + 1
   });
 });
