ebd0e004e07deb873b21eda3bbb72db0f0f4d05b
Sam Renfrew <contact@samrenfrew.co.uk>
2024-10-30 13:24:42 +0000

    Fix insertStyle tests

--- __tests__/modules/insertStyle.spec.ts
+++ __tests__/modules/insertStyle.spec.ts
@@ -9,16 +9,16 @@ describe("insertStyle", () => {
       document.body.firstChild.remove();
     }
   });
-  it("will exit if there is already an element with the ID provided so to not double add", () => {
+  it("will exit if there is already an element with the ID provided so to not double add", async () => {
     document.body.insertAdjacentHTML("beforeend", `<div id="${MOCK_STYLE_ID}">Test</div>`);
-    insertStyle(MOCK_STYLE_STRING, MOCK_STYLE_ID);
+    await insertStyle(MOCK_STYLE_STRING, MOCK_STYLE_ID);
     const element = document.querySelector(`#${MOCK_STYLE_ID}`);
     expect(element?.tagName).toBe("DIV");
     expect(element?.textContent).toBe("Test");
   });
 
-  it("will add a style element with the correct content and id", () => {
-    insertStyle(MOCK_STYLE_STRING, MOCK_STYLE_ID);
+  it("will add a style element with the correct content and id", async () => {
+    await insertStyle(MOCK_STYLE_STRING, MOCK_STYLE_ID);
     const style = document.querySelector(`#${MOCK_STYLE_ID}`);
     expect(style).toBeDefined();
     expect(style?.innerHTML).toBe(MOCK_STYLE_STRING);
