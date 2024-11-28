491bd269fb2cc8e43a58d9f39a131cb178f480a9
Sam Renfrew <contact@samrenfrew.co.uk>
2024-11-28 13:00:20 +0000

    Update webpack tests

--- __mocks__/webpackEntry.js
+++ __mocks__/webpackEntry.js
@@ -1,9 +1,19 @@
-import { RBTest } from "../dist";
+import { SPA } from "../dist";
 
-const Test = new RBTest("test", {
-  apply: () => {
-    console.log("apply!");
-  },
-  pageMatch: "test",
+const applyChanges = () => {
+  console.log("APPLYING!");
+  if (!!document.querySelector(".test--element")) {
+    console.log("already inserted");
+    return;
+  }
+  document.querySelector("main").insertAdjacentHTML("afterbegin", `<div class="test--element"></div>`);
+};
+
+const Test = new SPA("test", {
+  apply: applyChanges,
+  style: ".test--element { background:red;height:100px;width:100vw;}",
+  pageMatch: /ghd-duet/g,
+  watchForRemoval: ".test--element",
+  removeOnPageChange: ".test--element",
 });
 Test.init();
