f0d71fed6748be5c3d01f93fd53a83bed61a6373
Sam Renfrew <contact@samrenfrew.co.uk>
2025-01-27 17:18:44 +0000

    chore: Update docs

--- src/helpers/isDebug.ts
+++ src/helpers/isDebug.ts
@@ -1,6 +1,6 @@
 /**
- * Checks to see if `qa_mode=true` cookie is set, to enable advanced logging
+ * Checks to see if `jf_debug=true` cookie is set, to enable advanced logging
  *
- * @returns {boolean} True if the qa_mode cookie is enabled
+ * @returns {boolean} True if the jf_debug cookie is present and set to true
  */
 export const isDebug = (): boolean => /jf_debug=true/.test(document.cookie);
