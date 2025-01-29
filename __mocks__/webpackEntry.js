df49ca67d73414069ae1a9f2b49cd444ac6da4b2
Sam Renfrew <contact@samrenfrew.co.uk>
2025-01-29 15:58:17 +0000

    Update webpack

--- __mocks__/webpackEntry.js
+++ __mocks__/webpackEntry.js
@@ -1,167 +1,206 @@
-import Swiper from "swiper";
-import { Pagination } from "swiper/modules";
-import { createElement, elementReady, emitEvent, useSPA, waitFor } from "../dist";
+import { elementReady, emitEvent, useMutationObserver, useSPA } from "../dist";
 import style from "./style.scss";
 
 const jfExperiment = {
-  ticketId: "TOS_008738",
+  ticketId: "ALL_011184",
   variant: "A",
 };
 
-const STATE = {};
-
-const toggleModal = (open = true) => {
-  console.log("toggling modal:", open);
-  const target = document.querySelector(".TOS_008738--gallery");
-  if (!!!target) {
-    console.log("no toggle target");
-    return;
-  }
-  target.classList[!!open ? "add" : "remove"]("open");
-  if (!!open) bindCloseModal();
+const STATE = {
+  observer: useMutationObserver(jfExperiment.ticketId),
+  ready1: null,
+  ready2: null,
+  ready3: null,
+  // observer2: useMutationObserver(`${jfExperiment.ticketId}-pdp`),
 };
 
-const handleBodyClick = (event) => {
-  const { target } = event;
-  if (!!target.closest(".modal-default") && !target.closest(".close-modal")) {
-    console.log("clicked on modal");
-    return;
-  }
-  console.log("clicked on body");
-  document.body.removeEventListener("click", handleBodyClick);
-  toggleModal(false);
-};
+const isPDP = () => !!window.contexts?.includes("pdp");
+const isPLP = () => !!window.contexts?.includes("plp");
 
-const bindCloseModal = () => {
-  setTimeout(() => {
-    console.log("binding body click");
-    document.body.removeEventListener("click", handleBodyClick);
-    document.body.addEventListener("click", handleBodyClick);
-  }, 200);
-};
+const swapPlpImages = () => {
+  if (!isPLP()) return;
+  const productTiles = document.querySelectorAll(".b-product_tile-image_link");
 
-const initSwiper = () => {
-  const target = document.querySelector(".TOS_008738--gallery .swiper");
-  if (!!!target) {
-    console.log("no swiper target");
-    return;
-  }
-  const swiper = new Swiper(target, {
-    slidesPerView: 1,
-    modules: [Pagination],
-    centeredSlides: true,
-    centeredSlidesBounds: true,
-  });
-  console.log(swiper);
-};
+  if (productTiles.length === 0) return;
+  console.log({ productTiles });
+  productTiles.forEach((tile) => {
+    const mainImage = tile.querySelector(".b-product_tile-image.b-product_tile_alt_view-item");
+    const secondImage = tile.querySelector(".b-product_tile_alt_view-item.m-alt.b-product_tile-image");
 
-const createGallery = async () => {
-  try {
-    console.log("creating gallery");
-
-    const outerWrapper = createElement("div", {
-      class: "TOS_008738--gallery fixed inset-0 z-30 w-full h-screen flex items-start justify-center py-4 px-2 md:p-8",
-    });
-    // insert background shade
-    outerWrapper.insertAdjacentHTML(
-      "beforeend",
-      `<div class="fixed inset-0 w-full h-screen bg-black-shade z-header"></div>`
-    );
-
-    const innerWrapper = createElement("div", { class: "relative modal-default" });
-    innerWrapper.insertAdjacentHTML(
-      "beforeend",
-      `<div data-testid="modal-default-header" class="modal-default-header modal-default-header--background"><h2 data-testid="modal-title" class="w-10/12 w-full">Gallery</h2></div>
-      <div data-testid="close-modal-button-container" class="close-modal z-20 flex items-center content-center justify-between gap-7 text-blue absolute top-2 right-4 mx-0 my-0"> <button data-testid="close-modal-button"><svg data-v-c9eb2644="" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class=" xl"><path data-v-c9eb2644="" fill="currentColor" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"></path></svg></button></div>`
-    );
-
-    const innerContent = createElement("div", { class: "modal-content overflow-y-auto" });
-
-    // wrapper.insertAdjacentHTML("beforeend", `<div class="TOS_008738--close"><span class="sr-only">Close</span></div>`);
-
-    const swiperOuter = createElement("div", { class: "swiper" });
-    const swiperWrapper = createElement("div", { class: "swiper-wrapper" });
-
-    await waitFor(() => {
-      return (
-        document.querySelectorAll(
-          ".bg-white > .content-container > div:last-of-type > div:first-of-type > div:first-of-type > div > div:first-of-type .ssr-carousel .ssr-carousel-slide"
-        ).length > 0
-      );
-    });
-
-    const slides = [
-      ...document.querySelectorAll(
-        ".bg-white > .content-container > div:last-of-type > div:first-of-type > div:first-of-type > div > div:first-of-type .ssr-carousel .ssr-carousel-slide"
-      ),
-    ].map((slide) => {
-      return `<div class="swiper-slide">
-        ${slide.querySelector("picture")?.outerHTML || slide.querySelector("iframe")?.outerHTML}
-      </div>`;
-    });
-    if (slides.length == 0) {
-      console.log("issue with getting slides?");
+    if (!mainImage || !secondImage) {
       return;
     }
 
-    swiperWrapper.insertAdjacentHTML("beforeend", slides.join(""));
-    swiperOuter.insertAdjacentElement("beforeend", swiperWrapper);
-    swiperOuter.insertAdjacentHTML("beforeend", `<div class="swiper-pagination"></div>`);
-    innerContent.insertAdjacentElement("beforeend", swiperOuter);
-    innerWrapper.insertAdjacentElement("beforeend", innerContent);
-    outerWrapper.insertAdjacentElement("beforeend", innerWrapper);
+    if (
+      secondImage.classList.contains("ALL_011184--new-first-img") ||
+      mainImage.classList.contains("ALL_011184--new-first-img")
+    ) {
+      return;
+    }
+    try {
+      const mainImageClass = mainImage.className;
+      const secondImageClass = secondImage.className;
 
-    document.body.insertAdjacentElement("beforeend", outerWrapper);
+      mainImage.className = secondImageClass;
+      secondImage.className = mainImageClass;
 
-    initSwiper();
+      secondImage.classList.add("ALL_011184--new-first-img");
+
+      const mainImageParent = mainImage.parentNode;
+      const secondImageParent = secondImage.parentNode;
+      const mainImagePlaceholder = document.createElement("div");
+      const secondImagePlaceholder = document.createElement("div");
+
+      mainImageParent.replaceChild(mainImagePlaceholder, mainImage);
+      secondImageParent.replaceChild(secondImagePlaceholder, secondImage);
+      mainImageParent.replaceChild(secondImage, mainImagePlaceholder);
+      secondImageParent.replaceChild(mainImage, secondImagePlaceholder);
+    } catch (e) {
+      emitEvent("error", jfExperiment, e);
+    }
+  });
+};
+
+const swapThumbnails = () => {
+  if (!isPDP()) return;
+  console.log("swapping thumbnails");
+
+  const firstTarget = document.querySelector(
+    ".b-product_gallery-thumbs_track > .b-product_gallery-thumb:nth-of-type(1) > picture"
+  );
+  const secondTarget = document.querySelector(
+    ".b-product_gallery-thumbs_track > .b-product_gallery-thumb:nth-of-type(2) > picture"
+  );
+
+  swapFirstAndSecond(firstTarget, secondTarget);
+};
+
+const swapZoomModal = () => {
+  if (!isPDP()) return;
+  console.log("swapping zoom modal");
+
+  const firstOrSecondActive = !!document.querySelector(
+    `.b-product_gallery-thumbs_track > .b-product_gallery-thumb:nth-child(1)[class*="m-current"], .b-product_gallery-thumbs_track > .b-product_gallery-thumb:nth-child(2)[class*="m-current"]`
+  );
+
+  if (!firstOrSecondActive) return;
+
+  const firstTarget = document.querySelector(".pswp__container > .pswp__item:nth-child(2)");
+  const secondTarget = document.querySelector(".pswp__container > .pswp__item:nth-child(3)");
+
+  swapFirstAndSecond(firstTarget, secondTarget);
+};
+
+const swapFirstAndSecond = (firstTarget, secondTarget) => {
+  if (!!!firstTarget || !!!secondTarget) {
+    console.log("no first or second target");
+    return;
+  }
+
+  const firstSources = firstTarget.querySelectorAll("source");
+  const secondSources = secondTarget.querySelectorAll("source");
+
+  const firstSourcesArray = [...firstSources].map((source) => source.getAttribute("srcset"));
+  const secondSourcesArray = [...secondSources].map((source) => source.getAttribute("srcset"));
+
+  firstSources.forEach((source, i) => {
+    if (!source.classList.contains("swapped")) {
+      source.setAttribute("srcset", secondSourcesArray[i]);
+      source.classList.add("swapped");
+    }
+  });
+  secondSources.forEach((source, i) => {
+    if (!source.classList.contains("swapped")) {
+      source.setAttribute("srcset", firstSourcesArray[i]);
+      source.classList.add("swapped");
+    }
+  });
+
+  const firstImages = firstTarget.querySelectorAll("img");
+  const secondImages = secondTarget.querySelectorAll("img");
+
+  const firstImagesArray = [...firstImages].map((img) => img.getAttribute("src"));
+  const secondImagesArray = [...secondImages].map((img) => img.getAttribute("src"));
+
+  firstImages.forEach((img, i) => {
+    if (!img.classList.contains("swapped")) {
+      img.setAttribute("src", secondImagesArray[i]);
+      img.classList.add("swapped");
+    }
+  });
+  secondImages.forEach((img, i) => {
+    if (!img.classList.contains("swapped")) {
+      img.setAttribute("src", firstImagesArray[i]);
+      img.classList.add("swapped");
+    }
+  });
+};
+
+const swapCarouselImg = () => {
+  if (!isPDP()) return;
+
+  const firstTarget = document.querySelector(
+    ".b-product_slider-track > .b-product_slider-item:nth-of-type(1) > picture.b-product_image"
+  );
+  const secondTarget = document.querySelector(
+    ".b-product_slider-track > .b-product_slider-item:nth-of-type(2) > picture.b-product_image"
+  );
+
+  swapFirstAndSecond(firstTarget, secondTarget);
+};
+
+const watchForChanges = () => {
+  if (!isPLP()) return;
+  // console.log(STATE.observer.details.isObserving, "observing");
+  if (!!STATE.observer.details.isObserving) return;
+  console.log("watching for changes PLP");
+  /** @type {Element} The Target element to listen for changes to */
+  const target = document.querySelector("body");
+  if (!!!target) {
+    console.log("no watch target PLP");
+    return;
+  }
+
+  /** @type {MutationObserverInit} The MutationObserver options */
+  const config = { childList: true, subtree: true, attributes: true };
+
+  /** @type {MutationCallback} The Callback */
+  const callback = (mutations) => {
+    mutations.forEach((mutation) => {
+      mutation.addedNodes.forEach((node) => {
+        if (node.nodeType === 1) {
+          console.log(node.nodeType);
+          swapPlpImages();
+        }
+      });
+    });
+  };
+
+  /** Trigger the observer */
+  STATE.observer.observe(target, config, callback);
+};
+
+const applyChanges = async () => {
+  try {
+    // await reset();
+    swapPlpImages();
+    watchForChanges();
+    STATE.ready1 = elementReady(".b-product_image", swapCarouselImg, "ALL_011184--carousel_img");
+    STATE.ready2 = elementReady(".b-product_gallery-thumb picture", swapThumbnails, "ALL_011184--thumb_img");
+    STATE.ready3 = elementReady(".pswp__container img", swapZoomModal, "ALL_011184--zoom_img");
   } catch (e) {
     emitEvent("error", jfExperiment, e);
   }
 };
 
-const handleOpenClick = (/** @type {MouseEvent} */ event) => {
-  event.preventDefault();
-  event.stopImmediatePropagation();
-  const { target } = event;
-  const shouldOpen = target.closest(".touch-pan-y.touch-pan-x")?.classList.contains("cursor-zoom-in");
-  console.log("click gallery:", shouldOpen);
-
-  if (!shouldOpen) {
-    console.log("open click");
-    toggleModal(true);
-  } else {
-    console.log("close click");
-    toggleModal(false);
+const reset = async () => {
+  try {
+    await STATE.ready1?.destroy(0);
+    await STATE.ready2?.destroy(0);
+    await STATE.ready3?.destroy(0);
+  } catch (e) {
+    emitEvent("error", jfExperiment, e);
   }
-
-  // const gallery = document.querySelector(".TOS_008738--gallery");
-};
-
-const bindOpenGallery = () => {
-  console.log("binding gallery");
-
-  STATE.pictureReady = elementReady(
-    ".ssr-carousel-slide .touch-pan-y.touch-pan-x picture",
-    (el) => {
-      console.log("ready");
-      el.removeEventListener("click", handleOpenClick);
-      el.addEventListener("click", handleOpenClick);
-    },
-    "TOS_008738--picture"
-  );
-};
-
-const resetChanges = () => {
-  console.log("resetting");
-  STATE.pictureReady?.destroy();
-  document.querySelector(".TOS_008738--gallery")?.remove();
-};
-
-const applyChanges = () => {
-  // DO STUFF
-  console.log("applying changes");
-  createGallery();
-  bindOpenGallery();
 };
 
 (() => {
@@ -169,9 +208,9 @@ const applyChanges = () => {
     const Test = useSPA(jfExperiment.ticketId);
     Test.init({
       apply: applyChanges,
-      reset: resetChanges,
-      style: style,
-      location: /\/p\d+\/?(\?|$)/,
+      reset,
+      location: /\/men(\/ramskull|)\/(shirts|knitwear)/gi,
+      alwaysReset: true,
     });
   } catch (e) {
     emitEvent("error", jfExperiment, e);
